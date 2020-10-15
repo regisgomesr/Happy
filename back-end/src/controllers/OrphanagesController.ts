022976436bcce529d6890c59da02a4e04a858914
Regis Ribeiro <regisgomesr@gmail.com>
2020-10-15 17:12:06 -0300

    Adicionando Backend - Rotas ( GET e POST ) de Orfanatos

---
+++ back-end/src/controllers/OrphanagesController.ts
@@ -0,0 +1,53 @@
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import Orphanages from '../models/Orphanage';
+
+
+export default {
+
+  async index(request: Request, response: Response) {
+    const orphanagesRepository = getRepository(Orphanages);
+
+    const orphanages = await orphanagesRepository.find();
+
+    return response.json(orphanages);
+  },
+
+  async show(request: Request, response: Response) {
+    const { id } = request.params;
+
+    const orphanagesRepository = getRepository(Orphanages);
+
+    const orphanage = await orphanagesRepository.findOneOrFail(id);
+
+    return response.json(orphanage);
+  },
+
+  async create(request: Request, response: Response) {
+    const { 
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_weekends,
+     } = request.body;
+  
+     const orphanagesRepository = getRepository(Orphanages);
+  
+     const orphanage = orphanagesRepository.create({
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_weekends,
+     });
+  
+     await orphanagesRepository.save(orphanage);
+  
+    return response.status(201).json(orphanage);
+  } 
+}

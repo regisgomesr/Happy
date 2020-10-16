51afbcade17b826620f6be066abd1a677b404aae
Regis Ribeiro <regisgomesr@gmail.com>
2020-10-16 16:59:21 -0300

    Abstraindo Sidebar

--- front-end/src/pages/CreateOrphanage.tsx
+++ front-end/src/pages/CreateOrphanage.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Sidebar from '../components/Sidebar';
+
 import { Map, Marker, TileLayer } from 'react-leaflet';
 import L from 'leaflet';
-import { useHistory } from "react-router-dom";
 
-import { FiArrowLeft, FiPlus } from "react-icons/fi";
+import { FiPlus } from "react-icons/fi";
 
 import mapMarkerImg from '../images/map-marker.svg';
 
@@ -18,19 +19,11 @@ const happyMapIcon = L.icon({
 })
 
 export default function CreateOrphanage() {
-  const { goBack } = useHistory();
 
   return (
     <div id="page-create-orphanage">
-      <aside>
-        <img src={mapMarkerImg} alt="Happy" />
 
-        <footer>
-          <button type="button" onClick={goBack}>
-            <FiArrowLeft size={24} color="#FFF" />
-          </button>
-        </footer>
-      </aside>
+      <Sidebar />
 
       <main>
         <form className="create-orphanage-form">

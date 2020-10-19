68b1deb9ab11ed4a3d081e49e2ead84327be1dd6
Regis Ribeiro <regisgomesr@gmail.com>
2020-10-19 14:36:48 -0300

    Buscando dados da API - Todos os orfanatos ( OrphanagesMap )

--- mobile/src/pages/OrphanageDetails.tsx
+++ mobile/src/pages/OrphanageDetails.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { Image, View, ScrollView, Text, StyleSheet, Dimensions } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import { Feather, FontAwesome } from '@expo/vector-icons';
-
 import mapMarkerImg from '../images/map-marker.png';
 import { RectButton } from 'react-native-gesture-handler';
+import { useRoute } from '@react-navigation/native';
 
 export default function OrphanageDetails() {
+  const route = useRoute();
+
+  console.log(route.params);
+
   return (
     <ScrollView style={styles.container}>
       <View style={styles.imagesContainer}>
